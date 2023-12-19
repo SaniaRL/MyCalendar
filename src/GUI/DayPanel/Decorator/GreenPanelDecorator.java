86bed024a18a7e387e4114ecda3f00095ad4af88
SaniaRL <saniarunnfors@gmail.com>
2023-12-19 17:43:23 +0100

    Update

--- src/GUI/DayPanel/Decorator/GreenPanelDecorator.java
+++ src/GUI/DayPanel/Decorator/GreenPanelDecorator.java
@@ -5,16 +5,9 @@ import GUI.DayPanel.DayPanel;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
-public class GreenPanelDecorator extends DefaultPanelDecorator {
-
+public class GreenPanelDecorator extends DayPanelDecorator {
     public GreenPanelDecorator(DayPanel dayPanel){
-        super(dayPanel);
+        super(dayPanel, new Color(10, 240, 10), new Color(10, 200, 10));
         setColor();
-    }
-
-    @Override
-    public void setColor(){
-        dayPanel.setBackground(new Color(10, 240, 10));
-        dayPanel.setBorder(new LineBorder(new Color(10, 200, 10)));
     }
 }
--- src/GUI/DayPanel/Decorator/UglyPanelDecorator.java
+++ src/GUI/DayPanel/Decorator/UglyPanelDecorator.java
@@ -6,15 +6,8 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class UglyPanelDecorator extends DayPanelDecorator {
-
     public UglyPanelDecorator(DayPanel dayPanel){
-        super(dayPanel);
+        super(dayPanel, new Color(255, 155, 185), new Color(187, 130, 57));
         setColor();
-    }
-
-    @Override
-    public void setColor(){
-        dayPanel.setBackground(new Color(255, 155, 185));
-        dayPanel.setBorder(new LineBorder(new Color(187, 130, 57)));
     }
 }
